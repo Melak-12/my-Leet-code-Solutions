bdc957447f624187b1edd95f3b75e0205fc7bc4c
Melaku <88847281+Melaku4@users.noreply.github.com>
2022-10-05 15:02:03 +0300

    Update selectioin_sort.java

--- selectioin_sort.java
+++ selectioin_sort.java
@@ -1,34 +1,3 @@
-//{ Driver Code Starts
-import java.util.*;
-
-class GFG
-{
-	public static void main(String args[])
-	{
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		while(t>0)
-		{
-			int n = sc.nextInt();
-			int arr[] = new int[n]; 
-			for(int i=0;i<n;i++)
-			{
-				arr[i] = sc.nextInt();
-			}
-		
-			Solution obj = new Solution();
-			obj.selectionSort(arr, n);
-			
-			for(int i=0;i<n;i++)
-		    	System.out.print(arr[i]+" ");
-		    System.out.println();
-			t--;
-		}
-		
-	}
-}
-
-// } Driver Code Ends
 
 
 class Solution
