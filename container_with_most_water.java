8cae43f154dadc2a2aa523f31d4e7fac9bea90b7
Melaku <88847281+Melaku4@users.noreply.github.com>
2022-10-17 22:55:27 +0300

    Create container_with_most_water.java

---
+++ container_with_most_water.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int maxArea(int[] height) {
+        int area = 0;
+        int left = 0; 
+        int right = height.length - 1;
+        while (left < right) {
+            int width = right - left;
+          area = Math.max(area, Math.min(height[left], height[right]) * width);
+            if (height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return area;
+    }
+}
